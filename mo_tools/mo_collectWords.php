22eb9d7dee62e75d5539e85be03cf530a2e2d2e7
cata <cata@d440b0c1-a408-4afe-a3bf-7e427467e8b7>
2010-09-28 18:11:28 +0000

    Add GLP license notices

--- mo_tools/mo_collectWords.php
+++ mo_tools/mo_collectWords.php
@@ -1,5 +1,22 @@
-#!/usr/bin/php
 <?
+/**
+ * Copyright 2010 Cătălin Frâncu <cata@francu.com>
+ *
+ * This file is part of Civvic.
+ *
+ * Civvic is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Civvic is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Civvic.  If not, see <http://www.gnu.org/licenses/>.
+ **/
 
 /**
  * Parse the MO archive and extract text from PDFs. If the PDFs do contain
