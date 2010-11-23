e5e2acde5c15429b79e3d820c0cd29e7094a7782
cata <cata@d440b0c1-a408-4afe-a3bf-7e427467e8b7>
2010-11-23 19:01:25 +0000

    More tools to OCR and process old MO's.

--- mo_tools/mo_processAnalogPDF.php
+++ mo_tools/mo_processAnalogPDF.php
@@ -1,12 +1,15 @@
 <?php
 
+require_once('phplib/config.php');
 require_once('phplib/cmdLine.php');
+require_once('phplib/db.php');
+require_once('phplib/activeRecords.php');
 require_once('phplib/string.php');
 require_once('phplib/sys.php');
 
-define('TMP_DIR', '/tmp/');
-define('TESSDATA_PREFIX', '/home/cata/Desktop/tesseract-3.0/');
-define('TESSERACT_BINARY', '/home/cata/Desktop/tesseract-3.0/api/tesseract');
+$I_PREFIXES = array('ne', 're', 'micro', 'sub', 'bine', 'dez', 'nemai', 'pre', 'semi', 'supra');
+
+db_init(CONF_DATABASE_TOOLS);
 
 $args = cl_getArguments(array("input:", "output:"), array("input", "output"));
 if (!$args) {
@@ -14,7 +17,7 @@ if (!$args) {
 }
 
 $text = pdfToText($args['input']);
-$text = fixCharset($text);
+$text = fixDocument($text);
 file_put_contents($args['output'], $text) or die("Cannot write specified output file.\n");
 
 /*************************************************************************/
@@ -26,26 +29,95 @@ function usage() {
   exit();
 }
 
-function fixCharset($s) {
-  $s = str_replace(array('ä', 'Ä', 'ã', 'Ã', 'ş', 'Ş', 'ţ', 'Ţ'),
-                   array('ă', 'Ă', 'ă', 'Ă', 'ș', 'Ș', 'ț', 'Ț'),
+function fixDocument($s) {
+  $s = str_replace(array('ä', 'Ä', 'ã', 'Ã', 'å', 'Å', 'ş', 'Ş', 'ţ', 'Ţ'),
+                   array('ă', 'Ă', 'ă', 'Ă', 'ă', 'Ă', 'ș', 'Ș', 'ț', 'Ț'),
                    $s);
+  $s = preg_replace("/\b-\n\b/", '', $s);
   $s = preg_replace("/\b(S|s)(i|î)nt(em|eti|eți)?\b/", "$1unt$3", $s);
+  $s = preg_replace("/\bin\b/", 'în', $s);
+  $s = preg_replace("/\bIn\b/", 'În', $s);
+  $s = preg_replace("/\bsi\b/", 'și', $s);
+  $s = preg_replace("/\bSi\b/", 'Și', $s);
+  $s = preg_replace("/ ([;:?!.,])/", "$1", $s);
+  $s = fixWords($s);
   return $s;
+}
+
+/**
+ * Tokenize the string and try to improve each word individually.
+ **/
+function fixWords($s) {
+  $len = mb_strlen($s);
+  $result = '';
+  $token = '';
+  for ($i = 0; $i < $len; $i++) {
+    $c = string_getCharAt($s, $i);
+    if (string_isUnicodeLetter($c)) {
+      $token .= $c;
+    } else {
+      if ($token !== '') {
+        $result .= processToken($token);
+        $token = '';
+      }
+      $result .= $c;
+    }
+  }
+  return $result;
+}
+
+function processToken($token) {
+  global $I_PREFIXES;
+
+  if (mb_strlen($token) <= 1) {
+    return $token; // Ignore single letters
+  }
+
+  $origToken = $token;
+  // Replace î with â except (1) at the beginning or end of the token; (2) when prefixed by bine-, dez-, micro-, ne-, nemai-, pre-, re-, semi-, sub-, supra-
+  $i = 0;
+  $pos = false;
+  $len = mb_strlen($token);
+  while (($pos = mb_strpos($token, 'î', $i)) !== false) {
+    if (($pos > 0) && ($pos < $len - 1) && !in_array(mb_substr($token, 0, $pos), $I_PREFIXES)) {
+      $token = mb_substr($token, 0, $pos) . 'â' . mb_substr($token, $pos + 1);
+    }
+    $i = $pos + 1;
+  }
+  if ($token != $origToken) {
+    print "Replacing [$origToken] with [$token]\n";
+  }
+
+  //print "Token: [$token]\n";
+  $words = db_find(new Word(), "form = '{$token}'");
+  if (count($words)) {
+    return $token; // It's a valid word
+  }
+  $words = db_find(new Word(), "formUtf8General = '{$token}'");
+  if (count($words)) {
+    // TODO: try to find a frequent one
+    print "Replacing [{$token}] with [" . $words[0]->form . "]\n";
+    return $words[0]->form;
+  }
+  return $token; // No better replacement
 }
 
 function pdfToText($pdfFilename) {
   $result = '';
-  $ppmBaseName = tempnam(TMP_DIR, 'mo_');
-  sys_executeAndAssert("pdftoppm '$pdfFilename' $ppmBaseName");
+  $ppmBaseName = tempnam(CONF_TMP_DIR, 'mo_');
+  $command = "pdftoppm '$pdfFilename' $ppmBaseName";
+  print "Command: $command\n";
+  sys_executeAndAssert($command);
 
-  $files = scandir(TMP_DIR);
+  $files = scandir(CONF_TMP_DIR);
   foreach ($files as $file) {
-    $file = TMP_DIR . $file;
+    $file = CONF_TMP_DIR . $file;
     if (string_startsWith($file, $ppmBaseName)) {
       if (string_endsWith($file, '.ppm')) {
         print "Running tesseract on {$file}\n";
-        sys_executeAndAssert("TESSDATA_PREFIX='" . TESSDATA_PREFIX . "' " . TESSERACT_BINARY . " $file $file -l ron");
+        $command = "TESSDATA_PREFIX='" . CONF_TESSDATA_PREFIX . "' " . CONF_TESSERACT_BINARY . " $file $file -l ron";
+        print "Command: $command\n";
+        sys_executeAndAssert($command);
         $result .= file_get_contents("{$file}.txt");
         unlink("{$file}.txt");
       }
--- phplib/string.php
+++ phplib/string.php
@@ -1,5 +1,11 @@
 <?php
 
+$GLOBALS['string_unicode'] = array('ă', 'Ă', 'â', 'Â', 'á', 'Á', 'à', 'À', 'ä', 'Ä', 'å', 'Å', 'ç', 'Ç', 'ć', 'Ć', 'č', 'Č',
+                                   'é', 'É', 'è', 'È', 'ê', 'Ê', 'ë', 'Ë', 'ĕ', 'Ĕ', 'ğ', 'Ğ', 'í', 'Í', 'ì', 'Ì', 'î', 'Î',
+                                   'ï', 'Ï', 'ĩ', 'Ĩ', 'ñ', 'Ñ', 'ó', 'Ó', 'ò', 'Ò', 'ô', 'Ô', 'ö', 'Ö', 'õ', 'Õ', 'ř', 'Ř',
+                                   'š', 'Š', 'ș', 'Ș', 'ț', 'Ț', 'ș', 'Ș', 'ț', 'Ț', 'ú', 'Ú', 'ù', 'Ù', 'û', 'Û',
+                                   'ü', 'Ü', 'ŭ', 'Ŭ', 'ý', 'Ý', 'ÿ', 'Ÿ', 'ž', 'Ž');
+
 function string_startsWith($string, $substring) {
   $startString = substr($string, 0, strlen($substring));
   return $startString == $substring;
@@ -10,6 +16,14 @@ function string_endsWith($string, $substring) {
   $lenSubstring = strlen($substring);
   $endString = substr($string, $lenString - $lenSubstring, $lenSubstring);
   return $endString == $substring;
+}
+
+function string_getCharAt($s, $index) {
+  return mb_substr($s, $index, 1);
+}
+
+function string_isUnicodeLetter($char) {
+  return ctype_alpha($char) || in_array($char, $GLOBALS['string_unicode']);
 }
 
 ?>
