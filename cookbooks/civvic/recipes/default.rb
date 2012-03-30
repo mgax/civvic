713a9e648a30dbbd3df832fd2dcb7f5d92497476
Alex Morega <alex@grep.ro>
2012-03-31 00:22:36 +0300

    configure web app

--- cookbooks/civvic/recipes/default.rb
+++ cookbooks/civvic/recipes/default.rb
@@ -2,13 +2,19 @@ require_recipe "apt"
 require_recipe "apache2::mod_php5"
 
 
-#[''].each do |pkg|
-#  package pkg do
-#    :upgrade
-#  end
-#end
+['php5-cli', 'php5-curl', 'php5-mysql', 'smarty'].each do |pkg|
+  package pkg do
+    :upgrade
+  end
+end
 
 
 apache_site "default" do
   enable false
+end
+
+
+web_app "civvic" do
+  template "civvic.conf.erb"
+  docroot "#{@node[:vagrant][:directory]}/www"
 end
